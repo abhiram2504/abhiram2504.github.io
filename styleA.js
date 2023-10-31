b474c5665a026b70ec1b7e7d6a351f897a87e690
Abhiram Rustagi <yourgitemail@example.com>
2023-10-30 21:22:41 -0400

    removed scroll feature

--- styleA.js
+++ styleA.js
@@ -55,11 +55,4 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+
